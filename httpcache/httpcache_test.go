a5bde5a1512f3ab886937a5a422b8b3952b2b460
mmbros <server.mmbros@yandex.com>
2017-03-09 22:02:34 +0100

    added TestArenavisionSchedule test

--- httpcache/httpcache_test.go
+++ httpcache/httpcache_test.go
@@ -1,60 +1,93 @@
 package httpcache
 
 import (
+	"bufio"
 	"bytes"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/http/httputil"
 	"os"
 	"strings"
 	"testing"
 	"time"
 )
 
+var cacheFolder = "/tmp/httpcache_test"
 var logbuf bytes.Buffer
 
 func init() {
 	log.SetOutput(&logbuf)
 }
 
-//func TestDumpResponse(t *testing.T) {
+func myTestDumpResponse(t *testing.T) {
 
-//filename := "/tmp/test_dump_response.html"
-//url := `https://golang.org`
+	filename := "/tmp/test_dump_response.html"
+	url := `https://golang.org`
 
-//resp, err := http.Get(url)
-//if err != nil {
-//t.Fatal(err)
-//}
-//defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
 
-//buf, err := httputil.DumpResponse(resp, true)
-//if err != nil {
-//t.Fatal(err)
-//}
+	buf, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-//err = ioutil.WriteFile(filename, buf, 0644)
-//if err != nil {
-//t.Fatal(err)
-//}
+	err = ioutil.WriteFile(filename, buf, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-//// Read Dumped Response
+	// Read Dumped Response
 
-//f, err := os.Open(filename)
-//if err != nil {
-//t.Fatal(err)
-//}
-//defer f.Close()
+	f, err := os.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
 
-//buf2 := bufio.NewReader(f)
-//resp2, err := http.ReadResponse(buf2, nil)
-//if err != nil {
-//t.Fatal(err)
-//}
-//defer resp2.Body.Close()
+	buf2 := bufio.NewReader(f)
+	resp2, err := http.ReadResponse(buf2, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp2.Body.Close()
 
-//if resp2.Status != "200 OK" {
-//t.Errorf("Expecting status 200 OK, found %s\n", resp2.Status)
-//}
-//}
+	if resp2.Status != "200 OK" {
+		t.Errorf("Expecting status 200 OK, found %s\n", resp2.Status)
+	}
+}
+func TestArenavisionSchedule(t *testing.T) {
+
+	url := "http://arenavision.in/schedule"
+	ttl := 10 * time.Minute
+	client := NewTTL(cacheFolder, ttl)
+
+	// create a new http.Request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// set beget cookie
+	cookie := http.Cookie{
+		Name:    "beget",
+		Value:   "begetok",
+		Path:    "/",
+		Expires: time.Now().Add(19360000000),
+	}
+	req.AddCookie(&cookie)
+
+	// get the response
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+}
 
 func TestGet(t *testing.T) {
 
@@ -66,10 +99,9 @@ func TestGet(t *testing.T) {
 		logbuf.Reset()
 	}
 
-	folder := "/tmp/httpcache_test"
 	url := "http://www.google.com"
 	ttl := 100 * time.Millisecond
-	client := NewTTL(folder, ttl)
+	client := NewTTL(cacheFolder, ttl)
 
 	// clean cached file, if exists
 	os.Remove(client.LocalPath(url))
