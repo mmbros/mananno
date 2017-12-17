b64fbe0afdbf95032c7198a52823ce4570752d45
mmbros <server.mmbros@yandex.com>
2017-12-17 21:54:30 +0100

    update build.sh to compress binaries

--- build.sh
+++ build.sh
@@ -11,36 +11,62 @@ ASSET="asset"
 TMPL="tmpl"
 EXE="mananno2"
 
+
+BUILDFLAGS="-v"
+
+# Shrink your Go binaries with this one weird trick
+# https://blog.filippo.io/shrink-your-go-binaries-with-this-one-weird-trick/
+LDFLAGS="-s -w"
+
+LOG_PREFIX=">>> "
+
+function log {
+	echo "$LOG_PREFIX$1"
+}
+
 function usage {
 	echo "Usage: $(basename $0) {linux|rpi|tmpl-dev|tmpl-prod}"
 }
 
+function compress_exe {
+	if [ -x "$(command -v upx)" ]; then
+		log "Compressing executeble with UPX... "
+		upx $EXE
+		log "Compressing executeble with UPX... Done"
+	else
+		log "Skip compression: UPX not found!"
+
+	fi
+}
+
 function build_for_linux {
 	gen_tmpl_prod
-	echo -n "Building for Linux... "
-    go build -v -o $EXE  *.go
-	echo done
+	log "Building for Linux... "
+	go build $BUILDFLAGS -ldflags "$LDFLAGS" -o $EXE  *.go
+	log "Building for Linux... Done"
+	compress_exe
 }
 
 function build_for_rpi {
 	gen_tmpl_prod
-	echo -n "Building for Raspberry PI... "
-    GOOS=linux GOARCH=arm go build -v -o $EXE  *.go
-	echo done
+	log "Building for Raspberry PI... "
+	GOOS=linux GOARCH=arm go build $BUILDFLAGS -ldflags "$LDFLAGS" -o $EXE  *.go
+	log "Building for Raspberry PI... Done"
+	compress_exe
 }
 
 function gen_tmpl_dev {
-	echo -n "Generating templates for development... "
+	log "Generating templates for development... "
 	go-bindata -debug -pkg $PKG -prefix $ASSET -o $PKG/bindata.go $ASSET/...
 	gentmpl -d -c $PKG/gentmpl.conf -p $TMPL -o $PKG/templates.go
-	echo done
+	log "Generating templates for development... Done"
 }
 
 function gen_tmpl_prod {
-	echo -n "Generating templates for production... "
+	log "Generating templates for production... "
 	go-bindata -pkg $PKG -prefix $ASSET -o $PKG/bindata.go $ASSET/...
 	gentmpl -c $PKG/gentmpl.conf -p $TMPL -o $PKG/templates.go
-	echo done
+	log "Generating templates for production... Done"
 }
 
 case "$1" in
